6fe7646c1faa0c55751146dc1c39b9011546901d
dlehdgh000 <dlehdgh000@gmail.com>
2023-03-14 12:07:49 +0900

    Git Test v3

--- src/main/java/com/example/demo/Calculator2.java
+++ src/main/java/com/example/demo/Calculator2.java
@@ -2,11 +2,11 @@ package com.example.demo;
 
 import org.springframework.http.HttpHeaders;
 
-public class Calculator {
+public class Calculator2 {
 	
 	public int sum(int a,int b) {
 		
-		
+		System.out.println("두번째");
 		return a+b;
 	}
 }
---
+++ src/main/java/com/example/demo/Calculator3.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Calculator3 {
+public int sum(int a,int b) {
+		
+		System.out.println("세번째");
+		return a+b;
+}
+}
